f6de28dd4200b42c4c9dfbec9e2eee9b5c49abd5
jameshutchy <jameshutchy222@gmail.com>
2022-09-12 21:01:16 +1200

    nearly finished ass2 need class diagram

--- BCDE224 - Assessment 2 - James Hutchinson/SQL/Agora Trading-Queries.sql
+++ BCDE224 - Assessment 2 - James Hutchinson/SQL/Agora Trading-Queries.sql
@@ -17,6 +17,64 @@ FROM listing l
 INNER JOIN itemcategory c on c.categoryID=l.categoryID
 WHERE l.itemID = 1;
 
--- 
+-- COMPLEX QUERY -- create an invoice for items purchased
+DROP PROCEDURE IF EXISTS purchase_invoice;
+DELIMITER //
+CREATE PROCEDURE purchase_invoice( IN pID INT)
+BEGIN
+	SELECT b.businessName, AES_DECRYPT(b.banknumber, @encrypt_key) AS 'bankNumber', b.contactNumber, b.email, b.logo,
+	 l.sellerID, CONCAT(AES_DECRYPT(u.firstName, @encrypt_key), ' ', AES_DECRYPT(u.lastName, @encrypt_key)) AS 'sellerName', 
+     l.itemName, o.price, o.quantity, p.purchaseDate, (o.price * o.quantity) AS 'total'
+	 FROM purchase p
+	 INNER JOIN orderline o ON o.purchaseID = p.purchaseID
+	 INNER JOIN listing l ON l.itemID = o.itemID
+     INNER JOIN agorauser u ON u.userID = l.sellerID
+	 INNER JOIN business b ON u.businessID = b.businessID
+	 WHERE p.purchaseID = pID
+	 ORDER BY l.sellerID;
+END; //
+DELIMITER ;
+SET @purchaseID = 5;
+-- TEST TO CHECK HOW MANY ITEMS ARE ON ORDERLINE
+SELECT * FROM ORDERLINE WHERE purchaseID = @purchaseID;
+-- TEST THE STORED PROCEDURE
+CALL purchase_invoice(@purchaseID);
 
+-- SEARCH QUERIES
+-- SEARCH QUERY BUYER
+SET @searchWord = 'fresh';
+SELECT l.itemName, l.photo, l.price, l.inStock,  c.category, l.listingDate
+FROM listing l
+LEFT JOIN itemcategory c on c.categoryID=l.categoryID
+WHERE l.itemName LIKE CONCAT('%', @searchWord, '%') 
+OR c.category LIKE CONCAT('%', @searchWord, '%') 
+OR l.itemDescription LIKE CONCAT('%', @searchWord, '%');
 
+-- SEARCH QUERY SELLER
+SET @searchWord = 'fresh';
+SET @theSeller = 6;
+SELECT l.itemName, l.photo, l.price, l.inStock,  c.category, l.listingDate
+FROM listing l
+LEFT JOIN itemcategory c on c.categoryID=l.categoryID
+WHERE l.itemName LIKE CONCAT('%', @searchWord, '%') 
+OR c.category LIKE CONCAT('%', @searchWord, '%') 
+OR l.itemDescription LIKE CONCAT('%', @searchWord, '%') 
+AND l.sellerID = @theSeller;
+
+-- FOR SEARCH QUERY BUSINESS ADMIN 
+SET @searchWord = 'ap';
+SET @theBusiness = 1;
+SELECT li.itemName, li.photo, li.price, li.inStock, li.category, li.listingDate
+FROM (
+	SELECT l.itemName, l.sellerID, l.photo, l.price, l.inStock,  c.category, l.listingDate
+	FROM listing l
+	LEFT JOIN itemcategory c on c.categoryID=l.categoryID
+	WHERE l.itemName LIKE CONCAT('%', @searchWord, '%') 
+	OR c.category LIKE CONCAT('%', @searchWord, '%') 
+	OR l.itemDescription LIKE CONCAT('%', @searchWord, '%') 
+) as li 
+INNER JOIN (
+	SELECT userID FROM agorauser 
+	WHERE businessID = @theBusiness AND userRole = 'seller'
+) AS b
+WHERE li.sellerID = b.userID;
--- BCDE224 - Assessment 2 - James Hutchinson/SQL/AgoraTrading-InsertData.sql
+++ BCDE224 - Assessment 2 - James Hutchinson/SQL/AgoraTrading-InsertData.sql
@@ -5,14 +5,19 @@ MySQl Version 8.0
 -- INSERT DATA
 USE agoratrading;
 SET @encrypt_key = SHA2('j3k4567ki678ki12lp0', 256);
-
+ 
 -- BUSINESS
 INSERT INTO business (businessName, registrationNumber, bankNumber, contactNumber, email, logo, hqAddress, hqCity)
-VALUES ("Vel Vulputate Eu PC","7417252019957",AES_ENCRYPT("3016 683784 27580",@encrypt_key),"(112) 818-1686","sem.mollis@protonmail.edu", LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'),"978-4583 Euismod Rd.","Temuka"),
-  ("In Ornare Corporation","3762807333139",AES_ENCRYPT("3008 918774 47344",@encrypt_key),"(820) 876-9127","neque@protonmail.org",LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'), "2090 Dolor Road","Oamaru"),
-  ("Lacus Nulla Industries","2450458603259",AES_ENCRYPT("4839 6894 1534 3893",@encrypt_key),"(583) 210-0258","ornare.lectus.justo@hotmail.couk",LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'), "Ap #551-9049 Accumsan Av.","Blenheim"),
-  ("Etiam Vestibulum Industries","2924339899400",AES_ENCRYPT("3778 862136 61879",@encrypt_key),"(352) 882-1476","accumsan.sed@protonmail.net",LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'), "Ap #437-6175 Pede, Street","Feilding"),
-  ("Placerat Inc.","8722239375111",AES_ENCRYPT("558773 476442 6255",@encrypt_key),"(402) 331-2025","phasellus.dolor@icloud.com",LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'), "992-1201 Tristique Road","Invercargill");
+VALUES ("Vel Vulputate Eu PC","7417252019957",AES_ENCRYPT("3016 683784 27580",@encrypt_key),"(112) 818-1686","sem.mollis@protonmail.edu", 
+LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'),"978-4583 Euismod Rd.","Temuka"),
+  ("In Ornare Corporation","3762807333139",AES_ENCRYPT("3008 918774 47344",@encrypt_key),"(820) 876-9127","neque@protonmail.org",
+  LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'), "2090 Dolor Road","Oamaru"),
+  ("Lacus Nulla Industries","2450458603259",AES_ENCRYPT("4839 6894 1534 3893",@encrypt_key),"(583) 210-0258","ornare.lectus.justo@hotmail.couk",
+  LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'), "Ap #551-9049 Accumsan Av.","Blenheim"),
+  ("Etiam Vestibulum Industries","2924339899400",AES_ENCRYPT("3778 862136 61879",@encrypt_key),"(352) 882-1476","accumsan.sed@protonmail.net",
+  LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'), "Ap #437-6175 Pede, Street","Feilding"),
+  ("Placerat Inc.","8722239375111",AES_ENCRYPT("558773 476442 6255",@encrypt_key),"(402) 331-2025","phasellus.dolor@icloud.com",
+  LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'), "992-1201 Tristique Road","Invercargill");
 SELECT * FROM business;
 SELECT aes_decrypt(bankNumber, @encrypt_key) from business;
 
@@ -23,11 +28,16 @@ VALUES   ("Rachel123","","",AES_ENCRYPT("password1",@encrypt_key),"","","","Admi
   ("Cooper56","","",AES_ENCRYPT("password3",@encrypt_key),"","","","Admin","",3),
   ("Curran69","","",AES_ENCRYPT("password4",@encrypt_key),"","","","Admin","",4),
   ("Fred16","","",AES_ENCRYPT("password5",@encrypt_key),"","","","Admin","",5),
-  ("Daryl57",AES_ENCRYPT("Daryl",@encrypt_key), AES_ENCRYPT("Barrera",@encrypt_key), AES_ENCRYPT("password6",@encrypt_key),"Ap #803-797 Neque Avenue","Wanaka","(365) 366-3826","seller","maecenas.libero@google.net",1),
-  ("Colorado32",AES_ENCRYPT("Colorado",@encrypt_key), AES_ENCRYPT("Boyle",@encrypt_key), AES_ENCRYPT("password7",@encrypt_key),"Ap #220-5493 Montes, Rd.","Kapiti","(859) 101-5102","buyer","molestie.orci@icloud.ca",2),
-  ("Nicholas58",AES_ENCRYPT("Nicholas",@encrypt_key), AES_ENCRYPT("Tucker",@encrypt_key), AES_ENCRYPT("password8",@encrypt_key),"Ap #433-3388 Euismod Ave","Matamata","(528) 896-8042","seller","mattis.semper.dui@yahoo.edu",3),
-  ("Emi90",AES_ENCRYPT("Emi",@encrypt_key), AES_ENCRYPT("Vance",@encrypt_key), AES_ENCRYPT("password9",@encrypt_key),"Ap #440-1198 Enim St.","Balclutha","(486) 771-5619","seller","aliquet.nec@icloud.ca",4),
-  ("Raja123",AES_ENCRYPT("Raja",@encrypt_key), AES_ENCRYPT("Klein",@encrypt_key), AES_ENCRYPT("password11",@encrypt_key),"672-969 Sed Av.","Waitara","(810) 892-9482","buyer","aliquet@protonmail.net",4);
+  ("Daryl57",AES_ENCRYPT("Daryl",@encrypt_key), AES_ENCRYPT("Barrera",@encrypt_key), 
+  AES_ENCRYPT("password6",@encrypt_key),"Ap #803-797 Neque Avenue","Wanaka","(365) 366-3826","seller","maecenas.libero@google.net",1),
+  ("Colorado32",AES_ENCRYPT("Colorado",@encrypt_key), AES_ENCRYPT("Boyle",@encrypt_key), 
+  AES_ENCRYPT("password7",@encrypt_key),"Ap #220-5493 Montes, Rd.","Kapiti","(859) 101-5102","buyer","molestie.orci@icloud.ca",2),
+  ("Nicholas58",AES_ENCRYPT("Nicholas",@encrypt_key), AES_ENCRYPT("Tucker",@encrypt_key), 
+  AES_ENCRYPT("password8",@encrypt_key),"Ap #433-3388 Euismod Ave","Matamata","(528) 896-8042","seller","mattis.semper.dui@yahoo.edu",1),
+  ("Emi90",AES_ENCRYPT("Emi",@encrypt_key), AES_ENCRYPT("Vance",@encrypt_key), 
+  AES_ENCRYPT("password9",@encrypt_key),"Ap #440-1198 Enim St.","Balclutha","(486) 771-5619","seller","aliquet.nec@icloud.ca",4),
+  ("Raja123",AES_ENCRYPT("Raja",@encrypt_key), AES_ENCRYPT("Klein",@encrypt_key), 
+  AES_ENCRYPT("password11",@encrypt_key),"672-969 Sed Av.","Waitara","(810) 892-9482","buyer","aliquet@protonmail.net",4);
 SELECT * FROM agorauser;
 
 -- ITEM CATEGORY
@@ -38,14 +48,22 @@ SELECT * FROM itemcategory;
 
 -- ITEM (sellers 6,8,9) 
 INSERT INTO listing (itemName, itemDescription, price, inStock, listingDate, sellerID, categoryID, photo)
-VALUES ("1kg apples", "Fresh royal gala apples from hawkes bay", 3.39, 250,now(), 6, 1, LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
-		("1kg oranges", "Fresh naval oranges from hawkes bay", 2.69, 360,now(), 6, 1, LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
-		("1kg parsnip", "Fresh parsnip from hawkes bay", 5.99, 180,now(), 6, 2, LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
-		("brimstone tyre", "38.23 brimstone tyre high performance ", 74.99, 90,now(), 8, 15, LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
-		("RB20DE", " new Import from japan RB20DE engine", 2349.00, 8,now(), 8, 15, LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
-		("1kg Tasty Cheese", "Meadow fresh original tasty cheese", 23.00, 400,now(), 9, 6, LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
-        ("1kg Shredded Parmesan", "1kg of the finest meadow fresh shredded parmesan cheese", 26.00, 120,now(), 9,6, LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
-        ("Tray Blue Milk", "12 x 2L bottle blue top meadow fresh milk", 35.45, 200,now(), 9, 6, LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'));
+VALUES ("1kg apples", "Fresh royal gala apples from hawkes bay", 3.39, 250,now(), 6, 1, 
+LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
+		("1kg oranges", "Fresh naval oranges from hawkes bay", 2.69, 360,now(), 6, 1, 
+        LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
+		("1kg parsnip", "Fresh parsnip from hawkes bay", 5.99, 180,now(), 6, 2, 
+        LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
+		("brimstone tyre", "38.23 brimstone tyre high performance ", 74.99, 90,now(), 8, 15, 
+        LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
+		("RB20DE", " new Import from japan RB20DE engine", 2349.00, 8,now(), 8, 15, 
+        LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
+		("1kg Tasty Cheese", "Meadow fresh original tasty cheese", 23.00, 400,now(), 9, 6, 
+        LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
+        ("1kg Shredded Parmesan", "1kg of the finest meadow fresh shredded parmesan cheese", 26.00, 120,now(), 9,6, 
+        LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png')),
+        ("Tray Blue Milk", "12 x 2L bottle blue top meadow fresh milk", 35.45, 200,now(), 9, 6, 
+        LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/images/araLogo.png'));
  SELECT * FROM listing; 
   
   -- PURCHASE (buyers 7,10)
@@ -60,5 +78,5 @@ VALUES ("1kg apples", "Fresh royal gala apples from hawkes bay", 3.39, 250,now()
 		(74.99, 4, 4, 3), (23.00, 2, 6, 2), (35.45, 1, 8, 4),
         (26.00, 1, 7, 5), (26.00, 1, 7, 6), (26.00, 1, 7, 7), 
         (26.00, 1, 7, 8), (26.00, 1, 7, 9), (26.00, 1, 7, 10), 
-        (2200.00, 1, 5, 3), (3.39, 16, 1, 5), (26.00, 1, 7, 5);
+        (2200.00, 1, 5, 3), (3.39, 16, 1, 5), (23.00, 2, 6, 5);
   SELECT * FROM orderline;      
